95a56eb5d1fc95e78575c3a55807b9c11177d893
Kristaps Krūze <93118170+puspikselis@users.noreply.github.com>
2025-10-05 23:33:14 +0300

    make header clickable to homepage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 
 export function Header() {
   return (
     <header className="fixed inset-x-0 top-0 z-1 flex items-center justify-between bg-linear-to-b from-50% from-black to-transparent p-8">
-      <div className="flex gap-4">
+      <Link href="/" className="flex gap-4">
         <Avatar className="size-9 rounded-[0.75rem]">
           <AvatarImage className="rounded-none" src="/images/KK.png" />
           <AvatarFallback>KK</AvatarFallback>
@@ -13,7 +14,7 @@ export function Header() {
           <h1 className="font-semibold text-13/4 text-white">Kristaps Krūze</h1>
           <h2 className="font-medium text-12/4 text-dim-gray-100">Designer</h2>
         </div>
-      </div>
+      </Link>
       <Button className="inset-shadow-0-1-0 inset-shadow-white/4 h-9 rounded-full bg-nero-100 px-5 font-semibold text-12 text-white">
         Contact
       </Button>
